37a7190c865af5eb669842b1f98380e89bd4ce94
WiiliamMentzer <william.mentzer2k@gmail.com>
2022-08-29 07:49:47 -0700

    WIP pizza topping processor and TTD to pair with

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
+function acquireToppings(event) {
+  event.preventDefault();
+  let toppings = [];
+  document.querySelector('.toppings').checked.each(function() {
+    toppings.push(this.value);
+  });
+  console.log(toppings);
+}
+
 function Pizza(toppings, size) {
   this.toppings = toppings;
   this.size = size
 }
 
 let myPizza = new Pizza(["pepperoni", "bacon", "jalapenos"], "large");
+
+window.addEventListener("load", function() {
+  document.querySelector("form#toppingSelector").addEventListener("submit", acquireToppings);
+}); 
--- readme.md
+++ readme.md
@@ -9,3 +9,9 @@ Describe: Pizza()
 Test: "this should return the pizza object with toppings and size"
 Code: let myPizza = new Pizza([pepperoni, bacon, jalapenos], "large");
 Expected Output: Pizza{toppings:[pepperoni, bacon, jalapenos], size:"large" }
+
+Describe: acquireToppings()
+
+Test: "it should return the checkboxed values into an array 
+Code: console.log(toppings);
+Expected Output: ["pepperoni", "bacon", "jalapenos"]
